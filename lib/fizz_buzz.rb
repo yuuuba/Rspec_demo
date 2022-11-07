77a64e6f6c425554d49680e9f3941c1aa3467e4f
yuuuba <nsabu45@gmail.com>
2022-11-07 20:42:27 +0900

    添削していただくまで

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,12 +4,33 @@ class FizzBuzz
   def fizz?(n)
     # n % 3 == 0
     n.modulo(3).zero?
-  end  
+  end
+
   #5で割り切れる場合”Buzz”
   def buzz?(n)
     n.modulo(5).zero?
   end
+
   #15 で割り切れる場合”Fizz Buzz”
+  def fizzbuzz?(n)
+    n.modulo(15).zero?
+  end
 
   # TODO: fizz?(n) などのメソッドを活用して、今度は String として値を返すメソッドを定義する
+  def judgment?(n) #質問: ?はメソッドが何をするかわかりやすくするため？
+    (1..n).map do |num|
+      if fizzbuzz?(num) == true
+        "fizzbuzz"
+      elsif buzz?(num) == true
+        "buzz"
+      elsif fizz?(num) == true
+        "fizz"
+      else
+        num
+      end
+    end
+  end
+
+  number = FizzBuzz.new
+  puts number.judgment?(300)
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,29 +2,46 @@ require File.join(__dir__, '..', 'lib', 'fizz_buzz')
 
 RSpec.describe FizzBuzz do
   # HINT && 宿題: let を使うと、「fizz_buzz = FizzBuzz.new」の繰り返し記述が消せる
-  # let(:fizz_buzz) {}
+  let(:fizz_buzz) { FizzBuzz.new }
   
   describe '#fizz?' do
     it '3 を与えると、 true が返ってくる' do
-      fizz_buzz = FizzBuzz.new
       expect(fizz_buzz.fizz?(3)).to eq true
     end
     
     it '4 を与えると false が返ってくる' do
-      fizz_buzz = FizzBuzz.new
       expect(fizz_buzz.fizz?(4)).to eq false
     end
   end
 
   describe '#buzz?' do
     it '5を与えると、trueが返ってくる' do
-      fizz_buzz = FizzBuzz.new
       expect(fizz_buzz.buzz?(5)).to eq true
     end
 
     it '2 を与えると false が返ってくる' do
-      fizz_buzz = FizzBuzz.new
       expect(fizz_buzz.buzz?(2)).to eq false
     end
   end
+
+  describe '#fizzbuzz?' do
+    it '15を与えると、true が返ってくる' do
+      expect(fizz_buzz.fizzbuzz?(15)).to eq true
+    end
+
+    it '11を与えると、true が返ってくる' do
+      expect(fizz_buzz.fizzbuzz?(11)).to eq false
+    end
+  end
+
+  # describe '#judgment' do
+  #   it 'それぞれの数値をテストする' do
+  #     expect(fizz_buzz.judgment?(15)).to eq true
+  #   end
+
+  #   it 'それぞれの数値をテストする' do
+  #     expect(fizz_buzz.judgment?(14)).to eq false
+  #   end
+  # end
+
 end
